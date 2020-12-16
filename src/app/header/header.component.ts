d276cac0e52278daf876f9d3ec6e5b503737f37c
Jeo <jeo961@gmail.com>
2020-12-16 14:03:05 -0500

    added header open and close on mobile. Added rules.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  openNav:Boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  openNavToggle() :void {
+    this.openNav = !this.openNav;
+  }
 }
